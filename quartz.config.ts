3f4b4d759f6c2ff57a01e993f22e27155f826244
Crystal <lilmisscrystal81@gmail.com>
2026-02-13 01:03:38 -0500

    Update quartz.config.ts
    Changed Quartz title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Crystal's MCW Coder Hub",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
