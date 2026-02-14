4195240f53983cf75d3d6cb22dc699b532b7082b
Crystal <lilmisscrystal81@gmail.com>
2026-02-13 20:51:06 -0500

    Quartz sync: Feb 13, 2026, 8:51 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Google Sans",
+        header: "Schibsted Grotesk",
         body: "Google Sans",
         code: "Google Sans",
       },
