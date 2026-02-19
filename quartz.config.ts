54af6c0a6f2a76c5f47b9454940dd811c0b30ba6
Crystal <lilmisscrystal81@gmail.com>
2026-02-19 01:10:50 -0500

    Change dark color code in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -49,7 +49,7 @@ const config: QuartzConfig = {
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
-          dark: "#ebebec",
+          dark: "#FDF2F7",
           secondary: "#7b97aa",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
