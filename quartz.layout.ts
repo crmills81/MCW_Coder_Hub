a0e9f15cbfb61e01500011e540b22eac4be60ccb
Crystal <lilmisscrystal81@gmail.com>
2026-04-11 16:20:06 -0400

    Replace Backlinks with RecentNotes component

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -49,7 +49,7 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    Component.RecentNotes({ limit: 10 }),
   ],
 }
 
