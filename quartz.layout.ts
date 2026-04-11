3df552c5d9aeb725d9f731a469b3d0004e5dd819
Crystal <lilmisscrystal81@gmail.com>
2026-04-11 17:22:38 -0400

    Update TableOfContents settings for depth and collapse

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -49,8 +49,8 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents({ 
-  collapseByDefault: false, // This keeps it open
-  maxDepth: 3               // Adjust this to show deeper sub-headers (H2, H3, etc.)
+  collapseByDefault: true, // This keeps it open
+  maxDepth: 5               // Adjust this to show deeper sub-headers (H2, H3, etc.)
 }),
     Component.RecentNotes({ limit: 3 }),
   ],
