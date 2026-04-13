7c6fe7e7f34209040003bf9743cb4f2c3a0e6c02
Crystal <lilmisscrystal81@gmail.com>
2026-04-12 20:36:04 -0400

    Update footer link and copyright year

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -16,7 +16,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://www.happycovergirlcocovers.com">Crystal Mills{version}</a> © {2026}
         </p>
 {opts?.description && (
   <p class="footer-description">
