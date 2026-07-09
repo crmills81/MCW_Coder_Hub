9a5ced3b84fe31dfc093a43f127ccfad25d42d56
Crystal <lilmisscrystal81@gmail.com>
2026-07-09 01:32:11 -0400

    Remove Explorer plugin configuration
    Removed external plugin configuration for Explorer.

--- quartz.ts
+++ quartz.ts
@@ -3,12 +3,3 @@ import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/conf
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
-ExternalPlugin.Explorer({
-  mapFn: (node) => {
-    if (node.isFolder) {
-      node.displayName = "🗂️ " + node.displayName
-    } else {
-      node.displayName = "📃 " + node.displayName
-    }
-  },
-})
